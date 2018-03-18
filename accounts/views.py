7147c657fa3e99ea8b222c7d25750986ef410569
Amardhruva N Prabhu <flashrocket@users.noreply.github.com>
2018-03-18 22:28:40 +0530

    Added register email

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,22 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.views.generic.base import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.forms import RegistrationForm
+
+
+def send_user_register_email(request, user):
+    subject = "Registered User"
+    message = render_to_string("email/user_register_user.txt", {"user": user})
+    user_email = user.email
+    send_mail(subject, message, settings.ADMIN_EMAIL, [user_email])
+    message = render_to_string("email/user_register_editor.txt", {"user": user})
+    editor_email = User.objects.get(is_superuser=True).email
+    send_mail(subject, message, settings.ADMIN_EMAIL, [editor_email])
+
 
 # Create your views here.
 class ProfileView(LoginRequiredMixin, View):
@@ -20,6 +35,7 @@ class RegisterView(View):
             user.is_active=False
             user.save()
             user.groups.add(form.cleaned_data['user_type'])
+            send_user_register_email(request, user)
 
             return render(request, "registration/registration_success.html")
         return render(request, "registration/register.html", {"form":form})
